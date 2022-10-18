78a12b5f28d57174b860148cf7677aedb01cd067
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2022-10-18 12:00:14 +0300

    Case

--- MaasHesaplayici/src/FirstCaseMain.java
+++
@@ -1,6 +0,0 @@
-public class FirstCaseMain {
-	public static void main(String[] args) {
-		Employee employee = new Employee("Yalcin", 8500, 41, 2022);
-		System.out.println(employee.toString());
-	}
-}
